4ffbac06bb1e57912839bc71bb9c76f459dfa060
Hanjoon <hanjoon.choe@bespinglobal.com>
2024-06-21 15:44:55 +0900

    delete time print

--- main.py
+++ main.py
@@ -131,13 +131,11 @@ def sentence_embedding_batch(item: EmbeddingItem):
 
 @app.post("/api/assist/cross-encoder/similarity-scores")
 def sentence_embedding_batch(item: EmbeddingItem):
-    s = time.time()
     item = item.dict()
     data = item['data']
 
     query_doc_list = [[r['query'], r['passage']] for r in data]
     similarity_scores = assist_cross_encoder.compute_score(query_doc_list)
-    print(f'⏱️ process time of cross-encoder: {time.time() - s}')
 
     return JSONResponse({"similarity_scores": similarity_scores})
 
