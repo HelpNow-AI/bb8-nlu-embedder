2357e30ce50c9be33a9014a8f6a7e913e10c0ebf
jaehyeong-bespin <jaehyeong.an@bespinglobal.com>
2024-06-25 16:16:25 +0900

    add rerank

--- request.sh
+++ request.sh
@@ -1,5 +1,35 @@
 curl -X 'POST' \
-    'http://127.0.0.1:3000/encode' \
-    -H 'Content-Type: application/json' \
-    -d '{"sentences": ["hello world, how are you?"]}'
+  'http://localhost:3000/embedding_batch' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "sentences": [
+    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla", 
+    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
+    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
+    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
+    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla"
+  ]
 
+}'
+
+
+
+curl -X 'POST' \
+  'http://localhost:3000/rerank' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "data": [
+    {"query": "This is a apple", "passage": "This is a banana"},
+    {"query": "I really want to go home.", "passage": "Sorry.. But, I hate you."},
+    {"query": "How can I go to police station?", "passage": "Oh, Police station is over there."},
+    {"query": "This is a query", "passage": "This is a passage"},
+    {"query": "This is a query", "passage": "This is a passage"},
+    {"query": "This is a query", "passage": "This is a passage"},
+    {"query": "This is a query", "passage": "This is a passage"},
+    {"query": "This is a query", "passage": "This is a passage"},
+    {"query": "This is a query", "passage": "This is a passage"},
+    {"query": "This is a query", "passage": "This is a query"}
+  ]
+}'
--- service.py
+++ service.py
@@ -7,51 +7,51 @@ import numpy as np
 import bentoml
 
 
-SAMPLE_SENTENCES = [
-    "The sun dips below the horizon, painting the sky orange.",
-    "A gentle breeze whispers through the autumn leaves.",
-    "The moon casts a silver glow on the tranquil lake.",
-    "A solitary lighthouse stands guard on the rocky shore.",
-]
-
-MODEL_ID = "BAAI/bge-base-en-v1.5"
+EMBEDDER_MODEL_ID = "BAAI/bge-base-en-v1.5"
+RERANKER_MODEL_ID = "BAAI/bge-reranker-base"
 
 @bentoml.service(
     traffic={"timeout": 60},
     resources={"gpu": 1},
 )
-class SentenceTransformers:
+class ServiceEmbedder:
 
     def __init__(self) -> None:
         import torch
         from sentence_transformers import SentenceTransformer, models
+        from FlagEmbedding import FlagModel, FlagReranker
         
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model = SentenceTransformer(MODEL_ID, device=self.device)
-        print(f"Model '{MODEL_ID}' loaded on device: '{self.device}'.")
+        
+        # self.embedder_model = SentenceTransformer(EMBEDDER_MODEL_ID, device=self.device)
+        # print(f"Model '{EMBEDDER_MODEL_ID}' loaded on device: '{self.device}'.")
+        
+        self.assist_bi_encoder = FlagModel(
+            EMBEDDER_MODEL_ID,
+            query_instruction_for_retrieval="Represent this sentence for searching relevant passages: ",
+            use_fp16=False
+        ) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+        self.assist_cross_encoder = FlagReranker(RERANKER_MODEL_ID, use_fp16=False) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+        print(f"Models loaded on device: '{self.device}' 🔥")
+        
 
     @bentoml.api(batchable=True)
-    def encode(
+    def embedding_batch(
         self,
         sentences: t.List[str]
     ) -> np.ndarray:
         
-        return self.model.encode(sentences, normalize_embeddings=True, device=self.device)
-
-""" Request example
-
-curl -X 'POST' \
-  'http://localhost:3000/encode' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "sentences": [
-    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla", 
-    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
-    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
-    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
-    "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla"
-  ]
-}'
-"""
+        # return self.model.encode(sentences, normalize_embeddings=True, device=self.device)
+        return self.assist_bi_encoder.encode(sentences)
     
+    
+    @bentoml.api(batchable=True)
+    def rerank(
+        self,
+        data
+    ) -> np.ndarray:
+        
+        query_doc_list = [[r['query'], r['passage']] for r in data]
+        
+        # return self.model.encode(sentences, normalize_embeddings=True, device=self.device)
+        return self.assist_cross_encoder.compute_score(query_doc_list)
