f4bb869a16ec8f4d671bc372c38a0abf505a51ca
jaehyeong-bespin <jaehyeong.an@bespinglobal.com>
2024-06-25 16:46:47 +0900

    add endpoint

--- request.sh
+++ request.sh
@@ -1,22 +1,80 @@
+#!/bin/sh
+
 curl -X 'POST' \
-  'http://localhost:3000/embedding_batch' \
+  'http://localhost:3000/api/nlu/sentence-embedding' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "sentences": [
+  "query": "hello, my name is jaehyeong."
+}' > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "'/api/nlu/sentence-embedding' > 🟢"
+else
+  echo "'/api/nlu/sentence-embedding' > 🔴"
+fi
+
+
+curl -X 'POST' \
+  'http://localhost:3000/api/nlu/sentence-embedding-batch' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "data": [
+    {"text": "hello, my name is jaehyeong."}, {"text": "I really want to go home."}, {"text": "Sorry.. But, I hate you."}, {"text": "hahahahahah, blablablabla"}, 
+    {"text": "hello, my name is jaehyeong."}, {"text": "I really want to go home."}, {"text": "Sorry.. But, I hate you."}, {"text": "hahahahahah, blablablabla"},
+    {"text": "hello, my name is jaehyeong."}, {"text": "I really want to go home."}, {"text": "Sorry.. But, I hate you."}, {"text": "hahahahahah, blablablabla"},
+    {"text": "hello, my name is jaehyeong."}, {"text": "I really want to go home."}, {"text": "Sorry.. But, I hate you."}, {"text": "hahahahahah, blablablabla"},
+    {"text": "hello, my name is jaehyeong."}, {"text": "I really want to go home."}, {"text": "Sorry.. But, I hate you."}, {"text": "hahahahahah, blablablabla"}
+  ]
+}' > /dev/null 2>&1 
+
+if [ $? -eq 0 ]; then
+  echo "'/api/nlu/sentence-embedding-batch' > 🟢"
+else
+  echo "'/api/nlu/sentence-embedding-batch' > 🔴"
+fi
+
+
+
+curl -X 'POST' \
+  'http://localhost:3000/api/assist/sentence-embedding' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "query": "hello, my name is jaehyeong."
+}' > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "'/api/assist/sentence-embedding' > 🟢"
+else
+  echo "'/api/assist/sentence-embedding' > 🔴"
+fi
+
+
+curl -X 'POST' \
+  'http://localhost:3000/api/assist/sentence-embedding-batch' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "data": [
     "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla", 
     "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
     "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
     "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla",
     "hello, my name is jaehyeong.", "I really want to go home.", "Sorry.. But, I hate you.", "hahahahahah, blablablabla"
   ]
+}' > /dev/null 2>&1
 
-}'
-
+if [ $? -eq 0 ]; then
+  echo "'/api/assist/sentence-embedding-batch' > 🟢"
+else
+  echo "'/api/assist/sentence-embedding-batch' > 🔴"
+fi
 
 
 curl -X 'POST' \
-  'http://localhost:3000/rerank' \
+  'http://localhost:3000/api/assist/cross-encoder/similarity-scores' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
@@ -32,4 +90,10 @@ curl -X 'POST' \
     {"query": "This is a query", "passage": "This is a passage"},
     {"query": "This is a query", "passage": "This is a query"}
   ]
-}'
+}' > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "'/api/assist/cross-encoder/similarity-scores' > 🟢"
+else
+  echo "'/api/assist/cross-encoder/similarity-scores' > 🔴"
+fi
--- service.py
+++ service.py
@@ -7,8 +7,9 @@ import numpy as np
 import bentoml
 
 
-EMBEDDER_MODEL_ID = "BAAI/bge-base-en-v1.5"
-RERANKER_MODEL_ID = "BAAI/bge-reranker-base"
+NLU_EMBEDDER_MODEL_ID = "bespin-global/klue-sroberta-base-continue-learning-by-mnr"
+ASSIST_EMBEDDER_MODEL_ID = "BAAI/bge-base-en-v1.5"
+ASSIST_RERANKER_MODEL_ID = "BAAI/bge-reranker-base"
 
 @bentoml.service(
     traffic={"timeout": 60},
@@ -18,40 +19,66 @@ class ServiceEmbedder:
 
     def __init__(self) -> None:
         import torch
-        from sentence_transformers import SentenceTransformer, models
+        from sentence_transformers import SentenceTransformer
         from FlagEmbedding import FlagModel, FlagReranker
         
+        # Check device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        
-        # self.embedder_model = SentenceTransformer(EMBEDDER_MODEL_ID, device=self.device)
-        # print(f"Model '{EMBEDDER_MODEL_ID}' loaded on device: '{self.device}'.")
-        
-        self.assist_bi_encoder = FlagModel(
-            EMBEDDER_MODEL_ID,
+
+        # Load models
+        self.assist_embedder = FlagModel(
+            ASSIST_EMBEDDER_MODEL_ID,
             query_instruction_for_retrieval="Represent this sentence for searching relevant passages: ",
             use_fp16=False
-        ) # Setting use_fp16 to True speeds up computation with a slight performance degradation
-        self.assist_cross_encoder = FlagReranker(RERANKER_MODEL_ID, use_fp16=False) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+        ) 
+        self.assist_reranker = FlagReranker(ASSIST_RERANKER_MODEL_ID, use_fp16=False)
+        self.nlu_embedder =  SentenceTransformer(NLU_EMBEDDER_MODEL_ID, device=self.device)
         print(f"Models loaded on device: '{self.device}' 🔥")
         
-
-    @bentoml.api(batchable=True)
-    def embedding_batch(
+        
+    @bentoml.api(route="/api/nlu/sentence-embedding", batchable=True)
+    def nlu_embedd(
         self,
-        sentences: t.List[str]
+        query: str
     ) -> np.ndarray:
         
-        # return self.model.encode(sentences, normalize_embeddings=True, device=self.device)
-        return self.assist_bi_encoder.encode(sentences)
+        return self.nlu_embedder.encode(query, device=self.device)
+
+
+    @bentoml.api(route="/api/nlu/sentence-embedding-batch", batchable=True)
+    def nlu_embedd_batch(
+        self,
+        data
+    ) -> np.ndarray:
+        query_list = [r['text'] for r in data]
+        
+        return self.nlu_embedder.encode(query_list, device=self.device)
     
     
-    @bentoml.api(batchable=True)
-    def rerank(
+    @bentoml.api(route="/api/assist/sentence-embedding", batchable=True)
+    def assist_embedd(
+        self,
+        query: str
+    ) -> np.ndarray:
+        
+        return self.assist_embedder.encode_queries(query) # query_instruction_for_retrieval + query
+
+
+    @bentoml.api(route="/api/assist/sentence-embedding-batch", batchable=True)
+    def assist_embedd_batch(
+        self,
+        data: t.List[str]
+    ) -> np.ndarray:
+        
+        return self.assist_embedder.encode(data) 
+    
+    
+    @bentoml.api(route="/api/assist/cross-encoder/similarity-scores", batchable=True)
+    def assist_rerank(
         self,
         data
     ) -> np.ndarray:
         
         query_doc_list = [[r['query'], r['passage']] for r in data]
         
-        # return self.model.encode(sentences, normalize_embeddings=True, device=self.device)
-        return self.assist_cross_encoder.compute_score(query_doc_list)
+        return self.assist_reranker.compute_score(query_doc_list)
