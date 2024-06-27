a4742d22694c0a3d36d3a4aa0177a28111aaae82
Hanjoon <hanjoon.choe@bespinglobal.com>
2024-06-27 14:03:23 +0900

    add dynamic batcher

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ sequence = np.array(
 sequence = np.char.encode(sequence, "utf-8")
 print(f"Sequence: {sequence}")
 
-with ModelClient("localhost", "bb8-embedder-nlu", init_timeout_s=600.0) as nlu_client:
+with ModelClient("http://localhost", "bb8-embedder-nlu", init_timeout_s=600.0) as nlu_client:
     result_dict = nlu_client.infer_batch(sequence)
     embed_vector = [row.astype(float) for row in result_dict['embed_vectors']]
     print(embed_vector)
@@ -28,7 +28,7 @@ sequence = np.char.encode(sequence, "utf-8")
 print(f"Sequence: {sequence}")
 
 
-with ModelClient("34.64.251.218:8000", "bb8-embedder-assist", init_timeout_s=600.0) as assist_client:
+with ModelClient("http://localhost", "bb8-embedder-assist", init_timeout_s=600.0) as assist_client:
     result_dict = assist_client.infer_batch(sequence)
     embed_vector = np.array([row.astype(float) for row in result_dict['embed_vectors']])
     print(embed_vector)
@@ -58,6 +58,6 @@ passages = np.array(
 queries = np.char.encode(queries, "utf-8")
 passages = np.char.encode(passages, "utf-8")
 
-with ModelClient("localhost", "bb8-embedder-assist-crossencoder", init_timeout_s=600.0) as assist_client:
+with ModelClient("http://localhost", "bb8-embedder-assist-crossencoder", init_timeout_s=600.0) as assist_client:
     result_dict = assist_client.infer_batch(queries, passages)
     print(result_dict)
